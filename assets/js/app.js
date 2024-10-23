7ea40388baa147ca93c3f6c14f1d94501cc2b873
phuongthanhf6 <phuongthanh.laptrinh@gmail.com>
2024-10-23 16:50:30 +0700

    sd

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+function speakWord(button) {
+    // Tìm từ vựng liên kết với nút nhấn
+    const itemDiv = button.closest(".item");
+    const vocabElement = itemDiv.querySelector("#vocab");
+    const word = vocabElement.textContent.trim();
+
+    // Phát âm từ vựng
+    const msg = new SpeechSynthesisUtterance(word);
+    msg.lang = "en-US";
+    msg.voice =
+        speechSynthesis
+            .getVoices()
+            .find((voice) => voice.name.includes("Google US English")) || null;
+
+    window.speechSynthesis.speak(msg);
+    window.speechSynthesis.onvoiceschanged = function () {};
+}
